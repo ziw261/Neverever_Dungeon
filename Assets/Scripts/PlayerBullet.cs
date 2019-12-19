925a69f10f6a7408b3c00f3c6035eba61eee72c0
ziw261 <ziw261@ucsd.edu>
2019-12-18 18:25:40 -0800

    WIP
    Player moving logic implemented
    Player animation implemented
    Player shooting implemented
    Bullet particle effect implemented

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+    public float speed = 7.5f;
+    public Rigidbody2D theRB;
+    public GameObject bulletEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        theRB.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Instantiate(bulletEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+    private void OnBecameInvisible() {
+        
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public float moveSpeed = 5f;
+    private Vector2 _moveInput;
+
+
+    public Rigidbody2D theRb;
+    public Transform gunDir;
+
+    private Camera _theCam;
+
+    public Animator anim;
+
+    public GameObject bulletToFire;
+    public Transform firePoint;
+
+    public float timeBetweenShots;
+    private float _shotCounter;
+    
+    // Start is called before the first frame update
+    void Start() {
+        _theCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+        
+        // Balance diagonal moving speed
+        _moveInput.Normalize();
+        
+        //transform.position += new Vector3(_moveInput.x * Time.deltaTime * moveSpeed, _moveInput.y * Time.deltaTime * moveSpeed, 0f);
+
+        theRb.velocity = _moveInput * moveSpeed;
+
+        // Calculate direction
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = _theCam.WorldToScreenPoint(transform.localPosition);
+
+        if (mousePos.x < screenPoint.x) {
+            transform.localScale = new Vector3(-1f,1f,1f);
+            gunDir.localScale = new Vector3(-1f,-1f,1f);
+        } else {
+            transform.localScale = new Vector3(1f,1f,1f);
+            gunDir.localScale = new Vector3(1f,1f,1f);
+
+        }
+        
+        Vector2 offSet = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offSet.y, offSet.x) * Mathf.Rad2Deg;
+        gunDir.rotation = Quaternion.Euler(0,0, angle);
+
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            _shotCounter = timeBetweenShots;
+        }
+
+        if (Input.GetMouseButton(0)) {
+            _shotCounter -= Time.deltaTime;
+
+            if (_shotCounter <= 0) {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                _shotCounter = timeBetweenShots;
+            }
+        }
+        
+        
+        // Animation
+        if (_moveInput != Vector2.zero) {
+            anim.SetBool("isWalking",true);
+        } else {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
